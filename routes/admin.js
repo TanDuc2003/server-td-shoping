6eae568d55fc4bcbf734b8dcaa83711eee3c13aa
Nguyễn Tấn Đức <101697146+TanDuc2003@users.noreply.github.com>
2022-11-09 22:08:57 +0700

    finnis backend

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,7 @@ const express = require("express");
 const adminRouter = express.Router();
 const admin = require("../middleware/admin");
 const { Product } = require("../models/product");
+const Order = require("../models/order");
 
 //thêm sản phẩm
 adminRouter.post("/admin/add-product", admin, async (req, res) => {
@@ -42,4 +43,79 @@ adminRouter.post("/admin/delete-product", admin, async (req, res) => {
     res.status(500).json({ error: e.message });
   }
 });
+
+// lấy danh sách order
+adminRouter.get("/admin/get-orders", admin, async (req, res) => {
+  try {
+    const orders = await Order.find({});
+    res.json(orders);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+//cập nhật trạng thái giao hàng
+adminRouter.post("/admin/change-order-status", admin, async (req, res) => {
+  try {
+    const { id, status } = req.body;
+    let order = await Order.findById(id);
+    order.status = status;
+    order = await order.save();
+    res.json(order);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+//
+adminRouter.get("/admin/analytics", admin, async (req, res) => {
+  try {
+    const orders = await Order.find({});
+    let totalEarnings = 0;
+
+    for (let i = 0; i < orders.length; i++) {
+      for (let j = 0; j < orders[i].products.length; j++) {
+        totalEarnings +=
+          orders[i].products[j].quantity * orders[i].products[j].product.price;
+      }
+    }
+
+    //danh mục sản phẩm
+    let dienthoaiEarnings = await fetchCategoryWiseProduct("Điện Thoại");
+    let thietyeuEarnings = await fetchCategoryWiseProduct("Thiết Yếu");
+    let giadungEarnings = await fetchCategoryWiseProduct("Gia Dụng");
+    let sachEarnings = await fetchCategoryWiseProduct("Sách");
+    let thoitrangEarnings = await fetchCategoryWiseProduct("Thời Trang");
+    let petEarnings = await fetchCategoryWiseProduct("Pet");
+    let dientuEarnings = await fetchCategoryWiseProduct("Điện Tử");
+
+    let earnings = {
+      totalEarnings,
+      dienthoaiEarnings,
+      thietyeuEarnings,
+      giadungEarnings,
+      sachEarnings,
+      thoitrangEarnings,
+      petEarnings,
+      dientuEarnings,
+    };
+    res.json(earnings);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+async function fetchCategoryWiseProduct(category) {
+  let earnings = 0;
+  let categoryOrders = await Order.find({
+    "products.product.category": category,
+  });
+
+  for (let i = 0; i < categoryOrders.length; i++) {
+    for (let j = 0; j < categoryOrders[i].products.length; j++) {
+      earnings +=
+        categoryOrders[i].products[j].quantity *
+        categoryOrders[i].products[j].product.price;
+    }
+  }
+  return earnings;
+}
 module.exports = adminRouter;
