7425cae3dc2e6689234dfdb7a44417f8af9976cc
Nguyễn Tấn Đức <101697146+TanDuc2003@users.noreply.github.com>
2022-11-17 22:01:35 +0700

    fix

--- routes/user.js
+++ routes/user.js
@@ -50,7 +50,27 @@ userRouter.delete("/api/remove-from-cart/:id", auth, async (req, res) => {
           user.cart.splice(i, 1);
         } else {
           user.cart[i].quantity -= 1;
+          console.log(user.cart[i].quantity);
         }
+      }
+    }
+
+    user = await user.save();
+    res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+//xóa sản phấm có trong giỏ hàng
+userRouter.delete("/api/delete-from-cart/:id", auth, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    let user = await User.findById(req.user);
+
+    for (let i = 0; i < user.cart.length; i++) {
+      if (user.cart[i].product._id.equals(product._id)) {
+        user.cart.splice(i, 1);
       }
     }
 
